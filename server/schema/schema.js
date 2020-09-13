7412bb437fc7a58c61b3c88fd40795c4a393fb1b
nikhilbhide <nikhilbhide15@gmail.com>
2020-09-13 12:13:20 +0530

    added mutation for user type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -117,6 +117,30 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name:'Mutation',
+    fields:{
+        createUser:{
+            type:UserType,
+            args:{
+                //id:{type:GraphQLID},
+                name:{type:GraphQLString},
+                age:{type:GraphQLInt},
+                profession:{type:GraphQLString},
+            },
+            resolve(parent,args) {
+                let user = {
+                    name : args.name,
+                    age : args.age,
+                    profession : args.profession
+                }
+                return user
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query:RootQuery
+    query:RootQuery, 
+    mutation:Mutation
 })
